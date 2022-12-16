ddfb617a344ae1817b293b14ef2c3796488eb6aa
Yvette Ibarra <yvette.ibarra01@gmail.com>
2022-12-15 19:38:25 -0600

    update bar graphs

--- explore.py
+++ explore.py
@@ -27,7 +27,7 @@ def viz_stroke_percentage(df):
 
 def viz_hypertension_vs_stroke(df):    
     fig, ax = plt.subplots(figsize=(9, 7))
-    ax=sns.barplot(x="hypertension", y='stroke', data=df, palette='Blues',edgecolor='white',linewidth=3)
+    ax=sns.barplot(x="hypertension", y='stroke', data=df, palette='Blues',edgecolor='white',linewidth=3,ci=None)
     mean_line = df.stroke.mean()
     plt.axhline(mean_line, label='Stroke Rate', color='#0b559f',linestyle='-.', linewidth=3)
 
@@ -44,7 +44,7 @@ def viz_heart_hyper_stroke(train):
     fig, ax = plt.subplots(figsize=(16, 7))
 
     plt.subplot(122)
-    ax=sns.barplot(x="hypertension", y='stroke', data=train, palette='Blues',edgecolor='white',linewidth=3)
+    ax=sns.barplot(x="hypertension", y='stroke', data=train, palette='Blues',edgecolor='white',linewidth=3, ci=None)
     mean_line = train.stroke.mean()
     plt.axhline(mean_line, label='Stroke Rate', color='#0b559f',linestyle='-.', linewidth=3)
 
@@ -57,7 +57,7 @@ def viz_heart_hyper_stroke(train):
     plt.text(-.1, .055, 'Average Stroke Rate of the Population', fontsize = 15,color='black',rotation=0)
 
     plt.subplot(121)
-    ax2=sns.barplot(x="heart_disease", y='stroke', data=train, palette='Blues',edgecolor='white',linewidth=3)
+    ax2=sns.barplot(x="heart_disease", y='stroke', data=train, palette='Blues',edgecolor='white',linewidth=3,ci=None)
     mean_line = train.stroke.mean()
     plt.axhline(mean_line, label='Stroke Rate', color='#0b559f',linestyle='-.', linewidth=3)
 
@@ -65,7 +65,7 @@ def viz_heart_hyper_stroke(train):
     ax2.set_xlabel('Heart disease', fontsize=16)
     ax2.set_ylabel('Stroke Rate', fontsize=16)
     ax2.set_ylim(0, 0.200)
-    ax2.set_title('Heart Population', fontsize=30,color='#0b559f')
+    ax2.set_title('Heart Disease Population', fontsize=30,color='#0b559f')
 
     ax2.set_xticklabels(['No','Yes'],fontsize=15)
     plt.text(-.1, .055, 'Average Stroke Rate of the Population', fontsize = 15,color='black',rotation=0);
@@ -74,7 +74,7 @@ def viz_heart_hyper_stroke(train):
 def viz_gender_heart_stroke(train):    
     plt.figure(figsize=(9,7))
     ax = sns.barplot(data=train, x='gender_Male', y='stroke',hue='heart_disease',palette='Blues',
-                     edgecolor='white',linewidth=3)
+                     edgecolor='white',linewidth=3,ci=None)
     plt.text(0, .055, 'Average Stroke Rate of the Population', fontsize = 15,color='black',rotation=0)
     mean_line = train.stroke.mean()
     plt.axhline(mean_line, label='Stroke Rate', color='#0b559f',linestyle='-.', linewidth=3)
@@ -96,7 +96,7 @@ def viz_age_vs_stroke(train):
     ax.set_title('Population vs Age', fontsize=30,color='#0b559f')
 
     plt.subplot(122)
-    ax2 = sns.stripplot(data=train,y='age',x='stroke', alpha=0.5,jitter=True )
+    ax2 = sns.stripplot(data=train,y='age',x='stroke', alpha=0.5,jitter=True,edgecolor='white',linewidth=.5 )
     ax2.set(facecolor='floralwhite')
     ax2.set_xlabel('Stroke ', fontsize=16)
     ax2.set_ylabel('Age', fontsize=16)
